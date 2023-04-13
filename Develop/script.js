1cef6180790f6a1697c9486428f121cf2368514f
Marjorie Bautista <marjoriebautista@Marjoriesb.local>
2023-04-12 23:07:12 -0400

    local storage was added, still not reflecting once the page is refreshed

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+//PUT DATE IN HEADER
+var todayDate = dayjs();
+var desBox = document.querySelector('.description').value ;
+$('#currentDay').text(todayDate.format('MMMM D, YYYY'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,7 +13,30 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+ 
+  var saveButton = document.querySelector('.btn');
+  var toDoText = document.querySelector('.description');
+
+
+  // toDoText.textContent= toDoText;
+
+ saveButton.addEventListener('click', function(event) { 
+ event.preventDefault();
+
+  localStorage.setItem('toDoText', JSON.stringify(toDoText));
+ renderMessage();
+});
+
+function renderMessage(){
+  var test = JSON.parse(localStorage.getItem('toDoText'));
+  if( test!== null){
+    test.textContent = toDoText;  
+}
+}
+
+  
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the

8eb854df7aebba0b23d0c659c1838611835c1655
Marjorie Bautista <marjoriebautista@Marjoriesb.local>
2023-04-14 17:56:21 -0400

    the save buton work for all the siblings

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,8 @@
 // in the html.
 //PUT DATE IN HEADER
 var todayDate = dayjs();
-var desBox = document.querySelector('.description').value ;
 $('#currentDay').text(todayDate.format('MMMM D, YYYY'));
 
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,25 +12,95 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
  
-  var saveButton = document.querySelector('.btn');
-  var toDoText = document.querySelector('.description');
+ var desBox = document.querySelector('.description').value ;
+//funtion to save input 
+var dataInput = $(this).siblings('.description').val();
 
 
+
+
+ // USER CLICKS SAVE
+$('.saveBtn').on('click', function(event){
+    console.log('you clicked save');
+    event.preventDefault();
+  //var input text by user 
+    var dataInput = $(this).siblings('.description').val();
+  localStorage.setItem('dataInput', dataInput);
+})
+
+
+
+
+
+
+
+ //Function
+  //save data input
+  
+  // display data input after refresh
+
+
+//Save data in local storage
+//  function saveInput(dataInput){
+//   localStorage.setItem('dataInput', jason.stringify(dataInput));  
+// }
+//  var dataInput = readInput();
+// //Display saved area and returns array
+// function readInput(){
+//   var dataInput = localStorage.getItem('dataInput');
+//   if (dataInput !== null){
+//    return; 
+//   } else{
+//    return dataInput= JSON.parse(dataInput);
+//   }
+// }
+
+
+//  var saveButton = document.querySelector('.btn');
+//   var toDoText = document.querySelector('.description');
+//   var example;
+//////-----------ONE FUNCTION
+// saveButton.addEventListener('click', function(event){ 
+//   event.preventDefault();
+//   console.log ('test SAVE');  
+//   var toDoText= {
+// }
+// example = doc
+// localStorage.setItem('toDoText', JSON.stringify(toDoText));
+//       renderMessage();
+//  });     
+
+// function renderMessage(){
+//   var toDoText = JSON.parse(localStorage.getItem("toDoText"));
+//   // var test = JSON.parse(localStorage.getItem('toDoText'));
+//   if( toDoText!== null){
+//     var toDoText = JSON.parse(localStorage.getItem("toDoText"));
+//     toDoText.textContent = toDoText.text ;
+  
+// }
+// }
+//-------------FUNCTION
+//  saveButton.addEventListener('click', function(event){ 
+//   console.log ('test SAVE');
+//     event.preventDefault();
+//     localStorage.setItem('toDoText', JSON.stringify(toDoText));
+
+//     // localStorage.getItem('toDoText',);
+//       if( toDoText !== null ) {
+//         JSON.parse(localStorage.getItem(toDoText));
+//         console.log(toDoText); 
+// }});
+
+
+
+  
   // toDoText.textContent= toDoText;
+// function saveButton (){
+//     var input = document.querySelector('.description').value;
+//     input.innerHTML = (input);
 
- saveButton.addEventListener('click', function(event) { 
- event.preventDefault();
+// }
 
-  localStorage.setItem('toDoText', JSON.stringify(toDoText));
- renderMessage();
-});
-
-function renderMessage(){
-  var test = JSON.parse(localStorage.getItem('toDoText'));
-  if( test!== null){
-    test.textContent = toDoText;  
-}
-}
 
   
 
@@ -48,4 +116,4 @@ function renderMessage(){
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });
